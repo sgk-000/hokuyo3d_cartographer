3a3badfc0cf525e72e580505da573a801be6fac0
Shigeki Kobayashi <orzorzorz619@gmail.com>
2019-12-05 18:08:04 +0900

    conf changed

--- configuration_files/hokuyo3d_single.lua
+++ configuration_files/hokuyo3d_single.lua
@@ -35,7 +35,7 @@ options = {
   tracking_frame = "hokuyo3d_imu",
   published_frame = "hokuyo3d_link",
   odom_frame = "hokuyo3d_link",
-  publish_frame_projected_to_2d = true,
+  publish_frame_projected_to_2d = false,
   provide_odom_frame = false,
   use_odometry = false,
   use_nav_sat = false,
@@ -64,6 +64,8 @@ TRAJECTORY_BUILDER_3D.max_range = MAX_3D_LASER_RANGE
 TRAJECTORY_BUILDER_3D.min_range = 0.5
 TRAJECTORY_BUILDER_3D.num_accumulated_range_data = 1
 TRAJECTORY_BUILDER_3D.voxel_filter_size = 0.05
+TRAJECTORY_BUILDER_3D.use_online_correlative_scan_matching = false
+TRAJECTORY_BUILDER_3D.submaps.num_range_data = 50
 
 TRAJECTORY_BUILDER_3D.high_resolution_adaptive_voxel_filter.max_range = 10.
 TRAJECTORY_BUILDER_3D.low_resolution_adaptive_voxel_filter.max_range = MAX_3D_LASER_RANGE
@@ -72,7 +74,7 @@ TRAJECTORY_BUILDER_3D.motion_filter.max_time_seconds = 1.0
 TRAJECTORY_BUILDER_3D.motion_filter.max_distance_meters = 0.05
 TRAJECTORY_BUILDER_3D.motion_filter.max_angle_radians = 0.02
 
-TRAJECTORY_BUILDER_3D.submaps.num_range_data = 1
+TRAJECTORY_BUILDER_3D.submaps.num_range_data = 10
 TRAJECTORY_BUILDER_3D.submaps.high_resolution = 0.05
 TRAJECTORY_BUILDER_3D.submaps.low_resolution = 0.35
 TRAJECTORY_BUILDER_3D.submaps.high_resolution_max_range = 10.
